acced5317528ea544d6f722bfc0fe726bec16d69
Carlos Alvarez <carlosxviii@gmail.com>
2018-10-25 21:44:24 -0300

    configured with mongolab and mvc

--- handler.js
+++ handler.js
@@ -1,6 +1,5 @@
 'use strict'
-
-require('./models/SearchOrder')
+require('dotenv').config({ path: './.env' });
 const SearchOrderCtrl = require('./controllers/SearchOrderController')
 
 module.exports.create = (event, context, callback) => {
@@ -10,73 +9,16 @@ module.exports.create = (event, context, callback) => {
 
 module.exports.getOne = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
-
   return SearchOrderCtrl.getOne(event, context, callback)
-  /* connectToDatabase()
-    .then(() => {
-      Note.findById(event.pathParameters.id)
-        .then(note => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(note)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the note.'
-        }))
-    }) */
 }
 
-/* module.exports.getAll = (event, context, callback) => {
+ module.exports.getAll = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
+  return SearchOrderCtrl.getAll(event, context, callback)
+} 
 
-  connectToDatabase()
-    .then(() => {
-      Note.find()
-        .then(notes => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(notes)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the notes.'
-        }))
-    })
-} */
-
-/* module.exports.update = (event, context, callback) => {
+module.exports.update = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
+  return SearchOrderCtrl.update(event, context, callback)
+}
 
-  connectToDatabase()
-    .then(() => {
-      Note.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
-        .then(note => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(note)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the notes.'
-        }))
-    })
-} */
-
-/* module.exports.delete = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false
-
-  connectToDatabase()
-    .then(() => {
-      Note.findByIdAndRemove(event.pathParameters.id)
-        .then(note => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not fetch the notes.'
-        }))
-    })
-} */
--- models/SearchOrder.js
+++ models/SearchOrder.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const SearchOrderSchema = new mongoose.Schema({
   query: String,
   provider: String,
+  status: { type: String, default: 'processing'},
   options: {
     user: String,
     password: String
@@ -12,4 +13,11 @@ const SearchOrderSchema = new mongoose.Schema({
   callbackUrl: String
 })
 
+/*let SearchOrder
+try {
+  SearchOrder = mongoose.model('SearchOrder')
+} catch (e) {
+  SearchOrder = mongoose.model('SearchOrder', SearchOrderSchema)
+}
+module.exports = SearchOrder*/
 module.exports = mongoose.model('SearchOrder', SearchOrderSchema)
